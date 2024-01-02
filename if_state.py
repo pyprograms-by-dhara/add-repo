9993a23d51209a6965f68e851c02335cb65f97e0
pyprograms-by-dhara <154490272+pyprograms-by-dhara@users.noreply.github.com>
2024-01-02 16:24:25 +0530

    add new program

---
+++ if_state.py
@@ -0,0 +1,9 @@
+#if.....elif
+a=int(input("Enter first number ="))
+b=int(input("Enter second number ="))
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal")
+else:
+    print("a is greater then b")
