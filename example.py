b421d4e21304bc5d4c4a8a1b8f10a6e6123d99dc
pyprograms-by-dhara <154490272+pyprograms-by-dhara@users.noreply.github.com>
2024-01-04 10:45:27 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,15 @@
+x=50
+y=200
+z=250
+if x>y and x>z:
+    print(" x is max number")
+if y>x and y>z:
+    print(" y is max number")
+if z>x and z>y:
+    print(" z is max number")
+print("*"*100)
+s=20
+d=30
+p=50
+if s>d or p>d:
+    print("any one condition is true")
